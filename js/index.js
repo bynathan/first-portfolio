efbf3353d8ae1e14b03cd7f8da51322b16f49a8d
Nathan da Silva Lira <108292134+bynathan@users.noreply.github.com>
2022-08-04 13:59:45 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,58 +1,65 @@
+// -----------------------------------DARK/LIGHT----------------------------------------------
+
 const themeLight = document.getElementById('ThemeLight');
 const themeDark = document.getElementById('ThemeDark');
-const tabsButtonSkills = document.getElementsByClassName('tabsbuttontwo')
-const tabsButtonProjects = document.getElementsByClassName('tabsbuttonone')
-const logoDark = document.querySelectorAll('.dark')
-const logoLight = document.querySelectorAll('.dark')
-
-// -----------------------------------DARK/LIGHT----------------------------------------------
 
 const removeDarkAddLight = function(){
     const darksElements = document.querySelectorAll('.dark');
     for(let i = 0; i <= darksElements.length; i++ ){
-    darksElements[i].classList.remove('dark')
-    darksElements[i].classList.add('light')
+    darksElements[i].classList.remove('dark');
+    darksElements[i].classList.add('light');
     }
-}
-
+};
 const removeLightAddDark = function(){
 const lightsElements = document.querySelectorAll('.light');
 for(let i = 0; i <= lightsElements.length; i++ ){
-    lightsElements[i].classList.remove('light')
-    lightsElements[i].classList.add('dark')
+    lightsElements[i].classList.remove('light');
+    lightsElements[i].classList.add('dark');
     }
-}
+};
 
 themeLight.addEventListener('click', removeDarkAddLight);
-
 themeDark.addEventListener('click', removeLightAddDark);
 
 // -----------------------------------TABS----------------------------------------------------
 
-tabsButtonSkills[0].addEventListener('click', function() {
+const tabsButtonSkills = document.getElementsByClassName('tabsbuttontwo');
+const tabsButtonProjects = document.getElementsByClassName('tabsbuttonone');
 
-    const tabsProjects = document.getElementsByClassName('projectsgrid')
-    const tabsSkills = document.getElementsByClassName('skillsgrid')
+const noneAddAndRemoveProject = function() {
 
-    tabsProjects[0].classList.add('none')
-    tabsSkills[0].classList.remove('none')
-})
+    const tabsProjects = document.getElementsByClassName('projectsgrid');
+    const tabsSkills = document.getElementsByClassName('skillsgrid');
 
-tabsButtonProjects[0].addEventListener('click', function() {
+    tabsProjects[0].classList.remove('none');
+    tabsSkills[0].classList.add('none');
+};
+const noneAddAndRemoveSkills = function() {
 
-    const tabsProjects = document.getElementsByClassName('projectsgrid')
-    const tabsSkills = document.getElementsByClassName('skillsgrid')
+    const tabsProjects = document.getElementsByClassName('projectsgrid');
+    const tabsSkills = document.getElementsByClassName('skillsgrid');
 
-    tabsProjects[0].classList.remove('none')
-    tabsSkills[0].classList.add('none')
-})
+    tabsProjects[0].classList.add('none');
+    tabsSkills[0].classList.remove('none');
+};
 
-//------------------------------------ALERT----------------------------------------------------
+tabsButtonProjects[0].addEventListener('click', noneAddAndRemoveProject);
+tabsButtonSkills[0].addEventListener('click', noneAddAndRemoveSkills);
 
-logoDark[4].addEventListener('click', function(){
-    prompt('Sou lindo né?')
-})
+// -----------------------------------TABSSELECT----------------------------------------------
 
-logoLight[3].addEventListener('click', function(){
-    prompt('Sou lindo né?')
-})
+const buttonProjects = document.getElementsByClassName('tabsbuttonone')[0];
+const buttonSkills = document.getElementsByClassName('tabsbuttontwo')[0];
+
+const addAndRemoveSelectProjects = function(){
+    buttonSkills.classList.remove('select')
+    buttonProjects.classList.add('select')
+};
+
+const addAndRemoveSelectSkills = function(){
+    buttonProjects.classList.remove('select')
+    buttonSkills.classList.add('select')
+};
+
+buttonProjects.addEventListener('click', addAndRemoveSelectProjects);
+buttonSkills.addEventListener('click', addAndRemoveSelectSkills);
